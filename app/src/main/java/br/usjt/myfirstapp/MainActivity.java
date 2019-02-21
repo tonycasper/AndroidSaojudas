76d2527c75a5efd7e474def597acbd5c1fc3b029
arqdsis <arqdsis@MOOLI10309.animaedu.intranet>
2019-02-20 22:34:30 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/myfirstapp/MainActivity.java
@@ -0,0 +1,31 @@
+package br.usjt.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import br.usjt.myfirstapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //constante static para identifcar a mensagem
+    public final static String EXTRA_MESSAGE = "br.usjt.meuprimeiroapp.MESSAGE";
+    //será chamado quando o usuário clicar em Enviar
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+}
